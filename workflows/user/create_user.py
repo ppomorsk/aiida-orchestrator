b435c444f25fbeff5e70a41eb8c149ce0c7c7d04
Pawel Pomorski <ppomorsk@sharcnet.ca>
2023-12-05 23:55:02 -0500

    Add create_user workflow that can launch AiiDA jobs

--- workflows/user/create_user.py
+++ workflows/user/create_user.py
@@ -13,6 +13,7 @@ from orchestrator.workflows.utils import wrap_create_initial_input_form
 from products.product_types.user import UserInactive, UserProvisioning
 from products.product_types.user_group import UserGroup
 
+from subprocess import PIPE, run
 
 def user_group_selector() -> list:
     user_group_subscriptions = {}
@@ -74,6 +75,44 @@ def initialize_subscription(subscription: UserInactive, username: str, age: int,
         f"User {username} from group {subscription.user.group.group_name} ({subscription.affiliation})"
     )
     subscription = UserProvisioning.from_other_lifecycle(subscription, SubscriptionLifecycle.PROVISIONING)
+
+    import json
+
+    f=open("/home/ubuntu/aiida-orchestrator/clusterlogs/computers_aiida.json","r")
+    g=f.readlines()
+    f.close()
+
+    computers=json.loads(g[0])
+    computer_list=[]
+    for d in computers:
+        computer_list.append(d["computer"])
+
+
+    fairshare_list=[]
+    for cluster in computer_list:
+
+        f=open("/home/ubuntu/aiida-orchestrator/clusterlogs/"+cluster+".load.log","r")
+        g=f.readlines()
+        f.close()
+        last=g[-1].split(",")
+        fairshare_list.append(last[1])
+
+    maxfairshare=fairshare_list.index(max(fairshare_list))
+    computerselect=computer_list[maxfairshare]
+
+    cmd='python3 '+username+" "+computerselect
+
+    cmd=cmd.split()
+
+    result = run(cmd, stdout=PIPE, stderr=PIPE, text=True)
+
+    s=result.stdout
+    s=s[s.find("uuid")+6:]
+    uuid_val = s[:s.find(" ")]
+    s=s[s.find("pk")+4:]
+    pk_val=s[:s.find(")")]
+
+    subscription.user.age = int(pk_val)
 
     return {"subscription": subscription}
 
